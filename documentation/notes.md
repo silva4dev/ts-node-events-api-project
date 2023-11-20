6f800fe283bf73d523124d3b4b7a0baadedde9b5
silva4dev <web4dev.lucas@outlook.com>
2023-11-20 17:48:23 -0300

    test: change return to class

--- documentation/notes.md
+++ documentation/notes.md
@@ -9,6 +9,7 @@
 * Duplicate Code
 * Shotgun Surgery
 * Long Parameter List
+* Primitive Obsession
 
 # Design Patterns/Principles/Conventions
 * You Ain't Gonna Need It (YAGNI)
--- tests/check-last-event-status.test.ts
+++ tests/check-last-event-status.test.ts
@@ -18,16 +18,18 @@
 
 import { set, reset } from 'mockdate'
 
+type EventStatus = { status: string }
+
 class CheckLastEventStatus {
   constructor (
     private readonly loadLastEventRepository: LoadLastEventRepository
   ) {}
 
-  async perform ({ groupId }: { groupId: string }): Promise<string> {
+  async perform ({ groupId }: { groupId: string }): Promise<EventStatus> {
     const event = await this.loadLastEventRepository.loadLastEvent({ groupId })
-    if (event === undefined) return 'done'
+    if (event === undefined) return { status: 'done' }
     const now = new Date()
-    return event.endDate > now ? 'active' : 'inReview'
+    return event.endDate > now ? { status: 'active' } : { status: 'inReview' }
   }
 }
 
@@ -85,9 +87,9 @@ describe('CheckLastEventStatus', () => {
     const { sut, loadLastEventRepository } = makeSut()
     loadLastEventRepository.output = undefined
 
-    const status = await sut.perform({ groupId })
+    const eventStatus = await sut.perform({ groupId })
 
-    expect(status).toBe('done')
+    expect(eventStatus.status).toBe('done')
   })
 
   it('should return status active when now is before event end time', async () => {
@@ -96,9 +98,9 @@ describe('CheckLastEventStatus', () => {
       endDate: new Date(new Date().getTime() + 1)
     }
 
-    const status = await sut.perform({ groupId })
+    const eventStatus = await sut.perform({ groupId })
 
-    expect(status).toBe('active')
+    expect(eventStatus.status).toBe('active')
   })
 
   it('should return status inReview when now is after event end time', async () => {
@@ -107,8 +109,8 @@ describe('CheckLastEventStatus', () => {
       endDate: new Date(new Date().getTime() - 1)
     }
 
-    const status = await sut.perform({ groupId })
+    const eventStatus = await sut.perform({ groupId })
 
-    expect(status).toBe('inReview')
+    expect(eventStatus.status).toBe('inReview')
   })
 })
